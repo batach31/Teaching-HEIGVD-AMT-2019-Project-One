1d992dfb58d965296e8d9fdf3999a8cff5332a7a
unknown <batach31@hotmail.com>
2019-11-08 11:24:34 +0100

    updated FlightReservation

--- src/main/java/ch/heigvd/amt/projectone/model/FlightReservation.java
+++ src/main/java/ch/heigvd/amt/projectone/model/FlightReservation.java
@@ -6,4 +6,18 @@ public class FlightReservation
 {
     private Flight flight;
     private List<Customer> customers;
+
+    public FlightReservation(Flight flight, List<Customer> customers) {
+        this.flight = flight;
+        this.customers = customers;
+    }
+
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
 }
