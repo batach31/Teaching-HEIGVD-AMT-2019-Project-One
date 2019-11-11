402d0b02670cf9776b58dd797189600ba2bb8b89
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2019-11-11 19:31:11 +0100

    Doc test begin

--- src/test/ch/heigvd/amt/projectone/dao/CustomerManagerTest.java
+++ src/test/ch/heigvd/amt/projectone/dao/CustomerManagerTest.java
@@ -2,19 +2,28 @@ package heigvd.amt.projectone.dao;
 
 import ch.heigvd.amt.projectone.dao.CustomerManager;
 import ch.heigvd.amt.projectone.model.Customer;
+
 import org.junit.Test;
+
+import javax.ejb.EJB;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+
+
 public class CustomerManagerTest {
+
+    @EJB
+    CustomerManager customerManager;
+
 
 
     @Test
     public void itShouldBePossibleToGenerateCustomerInDB()
     {
         Customer Miguel = new Customer(1, "Ydmon", "Miguel", "Gouveia", 24, "pass123");
-        CustomerManager customerManager = new CustomerManager();
+
         customerManager.createCustomer(Miguel);
         Customer test = customerManager.getCustomerByPseudo("Ydmon");
         assertNotNull(test);
