d6d6ced24a8439f06f89474400c882e7f1a79cb8
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2019-11-11 10:23:51 +0100

    Update FlightReservationManager2

--- src/main/java/ch/heigvd/amt/projectone/dao/FlightReservationManager.java
+++ src/main/java/ch/heigvd/amt/projectone/dao/FlightReservationManager.java
@@ -2,12 +2,14 @@ package ch.heigvd.amt.projectone.dao;
 
 import ch.heigvd.amt.projectone.model.Customer;
 import ch.heigvd.amt.projectone.model.Flight;
+import ch.heigvd.amt.projectone.model.FlightReservation;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class FlightReservationManager
 {
@@ -40,9 +42,27 @@ public class FlightReservationManager
         return success;
     }
 
-    public boolean deleteReservation(FlightManager flightManager)
+    public boolean deleteReservation(Customer customer)
     {
+        boolean success = false;
+        int nbRow;
+        try{
+            Connection connection = dataSource.getConnection();
 
+            PreparedStatement sql = connection.prepareStatement("DELETE FROM flightreservation WHERE customer_id = ?");
+
+            sql.setObject(1, customer.getCustomer_id());
+            nbRow = sql.executeUpdate();
+            connection.close();
+
+            if(nbRow > 0)//if row are deleted, return true
+            {
+                success = true;
+            }
+        }catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+        return success;
     }
 
     public Flight listFlight(long id_customer)
